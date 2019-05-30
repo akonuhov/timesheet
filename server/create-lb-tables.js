6927a5b02ace93c62d3afe9bf352f75f31f9bea8
aleksandrkonuhov <konyuhov@pharma-soft.ru>
2019-05-30 10:08:16 +0300

    Добавил новые модели Worker, Subdivision

--- server/create-lb-tables.js
+++ server/create-lb-tables.js
@@ -1,6 +1,6 @@
 var server = require('./server');
 var ds = server.dataSources.tabel;
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+var lbTables = ['Worker', 'Subdivision'];
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
