9768b052b7df2f7f0614d68d5409f82dd3f854e2
aleksandrkonuhov <konyuhov@pharma-soft.ru>
2019-05-30 11:33:48 +0300

    Добавил модальные окна добавления Работника и Подразделения

--- client/src/pages/Subdivisions.vue
+++ client/src/pages/Subdivisions.vue
@@ -1,25 +1,60 @@
 <template>
   <layout-main>
-    <v-layout wrap>
-      <v-flex class="mb-4">
-        <h1 class="headline">Список подразделений</h1>
-      </v-flex>
-    </v-layout>
-    <v-layout wrap>
-      <v-flex>
-        <v-data-table
-          :headers="headers"
-          :items="subdivision"
-          class="elevation-1"
-        >
-          <template v-slot:items="props">
-            <td>{{ null }}</td>
-            <td>{{ null }}</td>
-            <td>{{ null }}</td>
-          </template>
-        </v-data-table>
-      </v-flex>
-    </v-layout>
+    <v-toolbar flat color="transparent" class="mb-2">
+      <v-toolbar-title>Список подразделений</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="searchSubdivisions"
+        append-icon="search"
+        label="Поиск сотрудников"
+        single-line
+      ></v-text-field>
+      <v-dialog v-model="dialogSaveSubdivision" max-width="500px">
+        <template v-slot:activator="{ on }">
+          <v-btn dark color="primary" class="ml-5" v-on="on">
+            Добавить
+            <v-icon right dark>add</v-icon>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title>
+            <span class="headline">Добавить нового сотрудника</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-layout column>
+                <v-flex>
+                  <v-text-field v-model="subdivision.name" label="Наименование"></v-text-field>
+                </v-flex>
+                <v-flex>
+                  <v-text-field v-model="subdivision.code" label="Код"></v-text-field>
+                </v-flex>
+                <v-flex>
+                  <v-text-field v-model="subdivision.address" label="Адрес"></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" flat @click="onClickCloseDialogSubdivisions">Отмена</v-btn>
+            <v-btn color="blue darken-1" flat @click="onClickSaveDialogSubdivisions">Добавить</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-toolbar>
+    <v-data-table
+      :headers="headers"
+      :items="subdivisionsList"
+      :search="searchSubdivisions"
+      class="elevation-1"
+    >
+      <template v-slot:items="props">
+        <td>{{ null }}</td>
+        <td>{{ null }}</td>
+        <td>{{ null }}</td>
+      </template>
+    </v-data-table>
   </layout-main>
 </template>
 
@@ -36,8 +71,23 @@ export default {
       { text: 'Номер', value: 'username' },
       { text: 'Адрес', value: 'email' }
     ],
-    subdivision: []
-  })
+    subdivisionsList: [],
+    subdivision: {
+      name: null,
+      code: null,
+      address: null
+    },
+    searchSubdivisions: null,
+    dialogSaveSubdivision: false
+  }),
+  methods: {
+    onClickCloseDialogSubdivisions () {
+      this.dialogSaveSubdivision = false
+    },
+    onClickSaveDialogSubdivisions () {
+
+    }
+  }
 }
 </script>
 
--- client/src/pages/Workers.vue
+++ client/src/pages/Workers.vue
@@ -1,27 +1,73 @@
 <template>
   <layout-main>
-    <v-layout wrap>
-      <v-flex class="mb-4">
-        <h1 class="headline">Список сотрудников</h1>
-      </v-flex>
-    </v-layout>
-    <v-layout wrap>
-      <v-flex>
-        <v-data-table
-          :headers="headers"
-          :items="workers"
-          class="elevation-1"
-        >
-          <template v-slot:items="props">
-            <td>{{ null }}</td>
-            <td>{{ null }}</td>
-            <td>{{ null }}</td>
-            <td>{{ null }}</td>
-            <td>{{ null }}</td>
-          </template>
-        </v-data-table>
-      </v-flex>
-    </v-layout>
+    <v-toolbar flat color="transparent" class="mb-2">
+      <v-toolbar-title>Список сотрудников</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="searchWorkers"
+        append-icon="search"
+        label="Поиск сотрудников"
+        single-line
+      ></v-text-field>
+      <v-dialog v-model="dialogSaveWorker" max-width="500px">
+        <template v-slot:activator="{ on }">
+          <v-btn dark color="primary" class="ml-5" v-on="on">
+            Добавить
+            <v-icon right dark>add</v-icon>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title>
+            <span class="headline">Добавить нового сотрудника</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-layout column>
+                <v-flex>
+                  <v-text-field v-model="worker.full_name" label="ФИО"></v-text-field>
+                </v-flex>
+                <v-flex>
+                  <v-text-field v-model="worker.in" label="Идентификационный номер"></v-text-field>
+                </v-flex>
+                <v-flex>
+                  <v-select :items="getSubdivisionList" label="Подразделение"></v-select>
+                </v-flex>
+                <v-flex>
+                  <v-text-field v-model="worker.position" label="Должность"></v-text-field>
+                </v-flex>
+                <v-flex>
+                  <v-text-field v-model="worker.timesheet" label="Рабочий табель"></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" flat @click="onClickCloseDialogWorker">Отмена</v-btn>
+            <v-btn color="blue darken-1" flat @click="onClickSaveDialogWorker">Добавить</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-toolbar>
+    <v-data-table
+      :headers="headers"
+      :items="workersList"
+      :search="searchWorkers"
+      class="elevation-1"
+    >
+      <template v-slot:items="props">
+        <td>{{ null }}</td>
+        <td>{{ null }}</td>
+        <td>{{ null }}</td>
+        <td>{{ null }}</td>
+        <td>{{ null }}</td>
+      </template>
+      <template v-slot:no-results>
+        <v-alert :value="true" color="error" icon="warning">
+          Ваш поиск для "{{ search }}" не найдено результатов.
+        </v-alert>
+      </template>
+    </v-data-table>
   </layout-main>
 </template>
 
@@ -40,8 +86,31 @@ export default {
       { text: 'Роль', value: 'realm' },
       { text: 'Пароль', value: 'password' }
     ],
-    workers: []
-  })
+    workersList: [],
+    worker: {
+      in: null,
+      subdivision: null,
+      subdivision_number: null,
+      full_name: null,
+      position: null,
+      timesheet: null
+    },
+    searchWorkers: null,
+    dialogSaveWorker: false
+  }),
+  computed: {
+    getSubdivisionList () {
+      return []
+    }
+  },
+  methods: {
+    onClickCloseDialogWorker () {
+      this.dialogSaveWorker = false
+    },
+    onClickSaveDialogWorker () {
+
+    }
+  }
 }
 </script>
 
